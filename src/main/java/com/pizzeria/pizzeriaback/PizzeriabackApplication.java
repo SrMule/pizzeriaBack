96c9bb2edfacd31ffca288ab9c7559e2a8ed761f
SrMule <eduardo.garciao@usm.cl>
2024-02-08 18:53:56 -0300

    inicialización del proyecto

---
+++ src/main/java/com/pizzeria/pizzeriaback/PizzeriabackApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeria.pizzeriaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriabackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzeria/pizzeriaback/PizzeriabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.pizzeriaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
